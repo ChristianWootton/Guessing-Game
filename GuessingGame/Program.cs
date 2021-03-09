d23dd0093e5f50a1403025b73967c1fbe809f92f
Christian Wootton <cwootton162@gmail.com>
2021-03-09 20:30:11 +0000

    Project release/completion
    Short project. Simple logic, with some error handling.

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuessingGame{
+    class GuessingGameLogic{
+        static void Main(string[] args){
+
+            Random randomNumberGenerator = new Random();
+            int numberToGuess = randomNumberGenerator.Next(0, 100);
+            Console.WriteLine(numberToGuess);
+
+            bool gameWon = false;
+
+            do
+            {
+                Console.WriteLine("Please guess a number between 0 and 100");
+                string playerGuess = Console.ReadLine();
+                //Convert User input to int (using Parse)
+                int guessedNumber = int.Parse(playerGuess);
+
+                //Basic error checking, test if guessedNumber is between 0/100
+                if (guessedNumber < 0 || guessedNumber > 100){
+                    Console.WriteLine("Number is not between 0 and 100. Try again!");
+                }
+                else if (guessedNumber > numberToGuess){
+                    Console.WriteLine("Too high, Guess again!");
+                }
+                else if (guessedNumber < numberToGuess){
+                    Console.WriteLine("Too low, Guess again!");
+                }
+                else if (guessedNumber == numberToGuess){
+                    gameWon = true;
+                    Console.WriteLine("Good guess! You win!");
+                }
+            } while (gameWon == false);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Guessing Game
  Basic Guessing Game using Clean Code techniques
+
+Using Robert C. Martins "Clean Code" as a guide to implement naming techniques.
+To also keep code clean, crisp with one goal. 
